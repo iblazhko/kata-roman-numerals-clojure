a0b00fa96c3dcfee1efed4041bf1e3a0b2308a36
Ivan Blazhko <iblazhko@fastmail.in>
2022-06-12 10:38:40 +0100

    test: (Red) 0 translates to empty string

--- test/kata_roman_numerals_clojure/core_test.clj
+++ test/kata_roman_numerals_clojure/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [kata-roman-numerals-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest romanize-0-translates-to-empty-string
+  (testing "0 translates to empty string"
+    (is (= (romanize 0)
+           ""))))
