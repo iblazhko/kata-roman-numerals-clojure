11905a675aac9bb901a8e89f67d0e2c769349960
Ivan Blazhko <iblazhko@fastmail.in>
2022-06-12 14:55:06 +0100

    test: (Red) Full set of single letter numerals

--- test/kata_roman_numerals_clojure/roman_numerals_test.clj
+++ test/kata_roman_numerals_clojure/roman_numerals_test.clj
@@ -2,20 +2,19 @@
   (:require [clojure.test :refer :all]
             [kata-roman-numerals-clojure.roman-numerals :refer [romanize]]))
 
+(defn run-test-cases
+  [test-name test-cases]
+  (testing test-name
+    (doseq [test-case test-cases]
+      (let [number (first test-case)
+            expected-romans (second test-case)]
+        (testing (str number " -> " expected-romans)
+          (is (= (romanize number) expected-romans)))))))
+
 (deftest romanize-0-translates-to-empty-string
   (testing "0 translates to empty string"
     (is (= (romanize 0)
            ""))))
-
-(deftest romanize-1-translates-to-I
-  (testing "1 -> I"
-    (is (= (romanize 1)
-           "I"))))
-
-(deftest romanize-5-translates-to-V
-  (testing "5 -> V"
-    (is (= (romanize 5)
-           "V"))))
 
 (deftest romanize-2-translates-to-II
   (testing "2 -> II"
@@ -26,3 +25,15 @@
   (testing "4 -> IV"
     (is (= (romanize 4)
            "IV"))))
+
+(deftest romanize-single-numeral
+  (run-test-cases
+   "Single letter roman numerals"
+   [[1, "I"],
+    [5, "V"],
+    [10, "X"],
+    [50, "L"],
+    [100, "C"],
+    [500, "D"],
+    [1000, "M"]]))
+
