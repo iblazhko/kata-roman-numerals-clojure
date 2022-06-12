e2f480592ed8559887b34c10d489abeb1c098d7b
Ivan Blazhko <iblazhko@fastmail.in>
2022-06-12 16:04:51 +0100

    refactor: Hide private members

--- src/kata_roman_numerals_clojure/roman_numerals.clj
+++ src/kata_roman_numerals_clojure/roman_numerals.clj
@@ -1,25 +1,26 @@
 (ns kata-roman-numerals-clojure.roman-numerals)
 
-(def buckets {1 "I",
-              5 "V",
-              10 "X",
-              50 "L",
-              100 "C",
-              500 "D",
-              1000 "M",
-              4 "IV",
-              9 "IX",
-              40 "XL",
-              90 "XC",
-              400 "CD",
-              900 "CM"})
+(def ^:private
+  buckets {1 "I",
+           5 "V",
+           10 "X",
+           50 "L",
+           100 "C",
+           500 "D",
+           1000 "M",
+           4 "IV",
+           9 "IX",
+           40 "XL",
+           90 "XC",
+           400 "CD",
+           900 "CM"})
 
-(defn find-largest-bucket
+(defn- find-largest-bucket
   [number]
   (let [bucket-size (apply max (filter #(<= % number) (keys buckets)))]
     [bucket-size (get buckets bucket-size)]))
 
-(defn process-bucket
+(defn- process-bucket
   [numerals reminder]
   (if (= reminder 0)
     (apply str numerals)
