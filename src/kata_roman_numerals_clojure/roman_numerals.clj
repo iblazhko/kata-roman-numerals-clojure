8ea8c314a99f28e9b98ce6926787ef0f15d60519
Ivan Blazhko <iblazhko@fastmail.in>
2022-06-12 14:57:22 +0100

    test: (Green) Full set of single letter numerals

--- src/kata_roman_numerals_clojure/roman_numerals.clj
+++ src/kata_roman_numerals_clojure/roman_numerals.clj
@@ -2,6 +2,11 @@
 
 (def buckets {1 "I",
               5 "V",
+              10 "X",
+              50 "L",
+              100 "C",
+              500 "D",
+              1000 "M",
               4 "IV"})
 
 (defn find-largest-bucket
